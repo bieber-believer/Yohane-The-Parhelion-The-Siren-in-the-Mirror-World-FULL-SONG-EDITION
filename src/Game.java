968e7f079ee232a2aaff7627f5ce695c9b6b4d6f
ape ish <dyjasyza@gmail.com>
2026-06-12 16:19:48 +0800

    created the main menu for the game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Game {
+    Scanner scanner = new Scanner(System.in);
+    private boolean exit = false;
+    private static char choice;
+
+    public Game(){
+
+    }
+
+    public void mainMenu(){
+        System.out.println("       ************************************************");
+        System.out.println("       *             Yohane The Parhelion!            *");
+        System.out.println("       *        The Siren in the Mirror World!        *");
+        System.out.println("       ************************************************");
+
+        System.out.println("                 [N]ew Game");
+        System.out.println("                 [S]tatus");
+        System.out.println("                 [Q]uit");
+
+        System.out.print("Your choice: ");
+        choice = scanner.next().toLowerCase().charAt(0);
+
+        System.out.println(choice);
+    }
 }
