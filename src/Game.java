182a2fb1be74e41a7d0c8cc45b0f2abc55418bd9
ape ish <dyjasyza@gmail.com>
2026-06-12 17:06:50 +0800

    create status display for main menu option

--- src/Game.java
+++ src/Game.java
@@ -24,4 +24,26 @@ public class Game {
 
         System.out.println(choice);
     }
+
+    public void status(){
+        System.out.println("       ************************************************");
+        System.out.println("                         Overall Status                ");
+        System.out.println("       ************************************************");
+
+        System.out.printf("        Times Chika was saved %17d times\n", 0);
+        System.out.printf("        Times You was saved %19d times\n", 0);
+        System.out.printf("        Times Riko was saved %18d times\n", 0);
+        System.out.printf("        Times Hanamaru was saved %14d times\n", 0);
+        System.out.printf("        Times Ruby was saved %18d times\n", 0);
+        System.out.printf("        Times Dia was saved %19d times\n", 0);
+        System.out.printf("        Times Kanan was saved %17d times\n", 0);
+        System.out.printf("        Times Mari was saved %18d times\n\n", 0);
+
+        System.out.printf("        Times Siren was defeated %14d times\n\n", 0);
+
+        System.out.printf("        No. of game overs %21d times\n\n", 0);
+
+        System.out.printf("        Total gold spent %22d gp\n", 6665);
+        System.out.println("       ************************************************");
+    }
 }
