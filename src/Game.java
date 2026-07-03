b19dae2b04d1ff792b045f851f6ebbfb245df895
ape ish <dyjasyza@gmail.com>
2026-07-03 15:02:26 +0800

    create chooseDungeon outline

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
 
 public class Game {
+    Scanner scanner = new Scanner(System.in);
     private boolean exit;
 
     public Game(){
         exit = false;
+    }
+
+    public void chooseDungeon(){
+        char choice;
+
+        System.out.println("Lailaps: Yohane! Where should we go now?");
+        System.out.println();
+        System.out.printf("HP: %d / 3                      Total Gold: %d GP", 0, 0);
+        System.out.println("Item on Hand: N/A");
+        System.out.println();
+
+        System.out.println("[1] Visit                     [I]nventory");
+        System.out.println("[2] Visit                     [S]ave and Quit");
+        System.out.println("[3] Visit");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Choice: ");
+        choice = scanner.next().charAt(0);
     }
 }
